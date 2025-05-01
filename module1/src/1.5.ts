d028f8b08a0fb866686208778afb3e76600127ea
mohammaduzzal <aalp72142@gmail.com>
2025-05-01 18:37:00 +0600

    added obj optional and literal type

---
+++ module1/src/1.5.ts
@@ -0,0 +1,13 @@
+// reference type ---->object
+const user :{
+    company : 'bright'; //when value become type called literal type
+    readonly firstName : string; //cannot assign value cuz its read only
+    middleName?: string; //optional type
+    lastName : string;
+    isMarried : boolean;
+} ={
+    company : 'bright',
+    firstName : 'mohammad',
+    lastName : 'uzz al',
+    isMarried: false
+}
