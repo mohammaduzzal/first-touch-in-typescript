41ae8ccc9333793582029564f1aa2b4bc1b37574
mohammaduzzal <aalp72142@gmail.com>
2025-05-01 09:58:00 +0600

    basic data type

---
+++ module1/src/1.4.ts
@@ -0,0 +1,30 @@
+// basic data type 
+
+// str
+const fullName : string = 'bright';
+
+// num
+const roll : number = 123;
+
+// bool
+let isAdmin : boolean = true
+
+// undefine 
+let x : undefined = undefined;
+
+// null 
+let y : null = null;
+
+// any time data type
+
+let a; ('you can assign any data str,bool,num.but don;t use anytime data type')
+
+// array
+let friends : string[] = ['rik', 'nik']
+
+let eligibleRoll : number[] = [1,3]
+
+
+// tuple ---> special type of array -->maintain the order -->type of value
+
+const coordinate : [number,string,boolean] = [1,'ki', true]
